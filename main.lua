56925d30bf4a026a0b0f6839f6a93dcb8786efc3
Ltven0mI <lt.ven0mi@gmail.com>
2014-11-17 19:40:34 +1000

    Changed a tiny bit of code in main.lua

--- main.lua
+++ main.lua
@@ -140,6 +140,7 @@ function love.keyreleased(key)
 end
 
 function love.load(arg)
+	math.randomseed(os.time())
 	main.loader.load(arg)
 end
 
@@ -161,62 +162,6 @@ end
 
 function love.resize(w,h)
 	main.loader.resize(w, h)
-end
-
-function love.run()
-	if love.math then
-		love.math.setRandomSeed(os.time())
-	end
-
-	if love.event then
-		love.event.pump()
-	end
-
-	if love.load then love.load(arg) end
-
-	-- We don't want the first frame's dt to include time taken by love.load.
-	if love.timer then love.timer.step() end
-
-	local dt = 0
-
-	-- Main loop time.
-	while true do
-		-- Process events.
-		if love.event then
-			love.event.pump()
-			for e,a,b,c,d in love.event.poll() do
-				if e == "quit" then
-					if not love.quit or not love.quit() then
-						if love.audio then
-							love.audio.stop()
-						end
-						return
-					end
-				end
-				love.handlers[e](a,b,c,d)
-			end
-		end
-
-		-- Update dt, as we'll be passing it to update
-		if love.timer then
-			love.timer.step()
-			dt = love.timer.getDelta()
-		end
-
-		-- Call update and draw
-		if love.update then love.update(dt) end -- will pass 0 if love.timer is disabled
-
-		if love.window and love.graphics and love.window.isCreated() then
-			love.graphics.clear()
-			love.graphics.origin()
-			if love.draw then love.draw() end
-			love.graphics.present()
-		end
-
-		if love.timer then love.timer.sleep(0.001) end
-	end
-
-	main.loader.run()
 end
 
 function love.textinput(text)
