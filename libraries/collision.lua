e7fa2119b977ed079d4a738244d247480d5f85cb
Ltven0mI <lt.ven0mi@gmail.com>
2014-11-25 20:09:33 +1000

    Upgraded collision.boundingBox().

--- libraries/collision.lua
+++ libraries/collision.lua
@@ -1,13 +1,13 @@
 collision = {}
 
-function collision.boundingBox(x1,y1,w1,h1,vx1,vy1,x2,y2,w2,h2,vx2,vy2)
-	if x1 and y1 and w1 and h1 and vx1 and vy1 and x2 and y2 and w2 and h2 and vx2 and vy2 then
+function collision.boundingBox(x1,y1,w1,h1,vx1,vy1,x2,y2,w2,h2)
+	if x1 and y1 and w1 and h1 and vx1 and vy1 and x2 and y2 and w2 and h2 then
 		local u1, d1, l1, r1 = y1, y1+h1, x1, x1+w1
 		local u2, d2, l2, r2 = y2, y2+h2, x2, x2+w2
 		local hx, hy = x1, y1
 		local side = ""
 
-		hy = hy+vy1
+		hx = hx-vx1
 		u1, d1, l1, r1 = hy, hy+h1, hx, hx+w1
 
 		if vy1 < 0 then
@@ -30,6 +30,7 @@ function collision.boundingBox(x1,y1,w1,h1,vx1,vy1,x2,y2,w2,h2,vx2,vy2)
 		end
 
 		hx = hx+vx1
+		hy = hy-vy1
 		u1, d1, l1, r1 = hy, hy+h1, hx, hx+w1
 
 		if vx1 < 0 then
@@ -41,7 +42,6 @@ function collision.boundingBox(x1,y1,w1,h1,vx1,vy1,x2,y2,w2,h2,vx2,vy2)
 				end
 			end
 		end
-
 		if vx1 > 0 then
 			if r1>l2 and r1<r2 and l1<l2 then --Colliding on the right
 				if (d1>u2 and d1<=d2 and u1<u2) or (u1<d2 and u1>=u2 and d1>d2) or (u1<u2 and d1>d2) or (u1>=u2 and u1<d2 and d1<=d2 and d1>u2) then --Checks if there is any collision on the x plane
@@ -51,10 +51,10 @@ function collision.boundingBox(x1,y1,w1,h1,vx1,vy1,x2,y2,w2,h2,vx2,vy2)
 				end
 			end
 		end
-
+		hy = hy+vy1
 		return hx, hy, vx1, vy1, side
 	else
-		debug.log("[ERROR] Incorrect call to function 'collision.boundingBox(x1,y1,w1,h1,vx1,vy1,x2,y2,w2,h2,vx2,vy2)'")
+		debug.log("[ERROR] Incorrect call to function 'collision.boundingBox(x1,y1,w1,h1,vx1,vy1,x2,y2,w2,h2)'")
 		return 0, 0, 0, 0
 	end
 end
