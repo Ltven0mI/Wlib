2cf0e7ae06805f8f334cb5c5a8f1074d83f18738
Ltven0mI <lt.ven0mi@gmail.com>
2014-12-09 15:42:48 +1000

    Changed some code in main.lua

--- main.lua
+++ main.lua
@@ -1,104 +1,107 @@
 main = {}
-main.loader = require "loader"
+
+require("loader")
 
 -- Variables --
 main.debugMode = true
+main.colDebug = false
 main.font = love.graphics.getFont()
 main.width = love.graphics.getWidth()
 main.height = love.graphics.getHeight()
 
 -- Callbacks --
 function love.draw()
-	main.loader.draw()
+	loader.draw()
 end
 
 function love.focus(f)
-	main.loader.focus(f)
+	loader.focus(f)
 end
 
 function love.gamepadaxis(joystick,axis,value)
-	main.loader.gamepadaxis(joystick, axis, value)
+	loader.gamepadaxis(joystick, axis, value)
 end
 
 function love.gamepadpressed(joystick,button)
-	main.loader.gamepadpressed(joystick, button)
+	loader.gamepadpressed(joystick, button)
 end
 
 function love.gamepadreleased(joystick,button)
-	main.loader.gamepadreleased(joystick, button)
+	loader.gamepadreleased(joystick, button)
 end
 
 function love.joystickadded(joystick)
-	main.loader.joystickadded(joystick)
+	loader.joystickadded(joystick)
 end
 
 function love.joystickaxis(joystick,axis,value)
-	main.loader.joystickaxis(joystick, axis, value)
+	loader.joystickaxis(joystick, axis, value)
 end
 
 function love.joystickhat(joystick,hat,direction)
-	main.loader.joystickhat(joystick, hat, direction)
+	loader.joystickhat(joystick, hat, direction)
 end
 
 function love.joystickpressed(joystick,button)
-	main.loader.joystickpressed(joystick, button)
+	loader.joystickpressed(joystick, button)
 end
 
 function love.joystickreleased(joystick,button)
-	main.loader.joystickreleased(joystick, button)
+	loader.joystickreleased(joystick, button)
 end
 
 function love.joystickremoved(joystick)
-	main.loader.joystickremoved(joystick)
+	loader.joystickremoved(joystick)
 end
 
 function love.keypressed(key,isrepeat)
-	main.loader.keypressed(key, isrepeat)
+	loader.keypressed(key, isrepeat)
 end
 
 function love.keyreleased(key)
-	main.loader.keyreleased(key)
+	loader.keyreleased(key)
 end
 
 function love.load(arg)
 	math.randomseed(os.time())
-	main.loader.load(arg)
+	loader.load(arg)
 end
 
 function love.mousefocus(f)
-	main.loader.mousefocus(f)
+	loader.mousefocus(f)
 end
 
 function love.mousepressed(x,y,button)
-	main.loader.mousepressed(x, y, button)
+	loader.mousepressed(x, y, button)
 end
 
 function love.mousereleased(x,y,button)
-	main.loader.mousereleased(x, y, button)
+	loader.mousereleased(x, y, button)
 end
 
 function love.quit()
-	main.loader.quit()
+	loader.quit()
 end
 
 function love.resize(w,h)
 	main.width = w
 	main.height = h
-	main.loader.resize(w, h)
+	loader.resize(w, h)
 end
 
 function love.textinput(text)
-	main.loader.textinput(text)
+	loader.textinput(text)
 end
 
 function love.threaderror(thread,errorstr)
-	main.loader.threaderror(thread, errorstr)
+	loader.threaderror(thread, errorstr)
 end
 
 function love.update(dt)
-	main.loader.update(dt)
+	loader.update(dt)
+	if collectgarbage("count")/1024 > 512 then collectgarbage("collect") end
 end
 
 function love.visible(v)
-	main.loader.visible(v)
+	loader.visible(v)
 end
